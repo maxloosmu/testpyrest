8f5fd367be54a50bb8d6e58fc882a33a1f9b6522
(Max) Loo Pin Mok <maxloo@smu.edu.sg>
2022-06-21 22:23:10 +0800

    amended app/main.py, added folder /circleci/

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # https://www.geeksforgeeks.org/deploy-python-flask-app-on-heroku/
 # https://devcenter.heroku.com/articles/python-pip
+# https://devcenter.heroku.com/articles/build-docker-images-heroku-yml
 
 from flask import Flask
 
