7a1d39f9cfc97c33f21bae57c78047080d86f5a3
Max Loo <maxloo@smu.edu.sg>
2022-07-09 14:47:49 +0800

    in folder /pyrest/more/test1/, changed reading, 001, 002, in folder /pyrest/more/test1/test2/, added 001, 002

--- pyrest/more/test1/reading.py
+++ pyrest/more/test1/reading.py
@@ -2,11 +2,11 @@
 
 import os, json
 from os.path import isfile, join, getmtime
-workdir = "/mnt/c/Users/Max/src/testpyrest/pyrest/more/test1"
+workdir = os.getcwd()
 
 def getjson(pathin):
   with open(pathin, "r") as read_file:
-    data = json.loads(read_file)
+    data = json.load(read_file)
     data['jsonfile'] = pathin
     data['modtime'] = getmtime(pathin)
   return data
@@ -14,11 +14,11 @@ def getjson(pathin):
 def read_all():
   vuedirs = [ getjson(join(workdir,f,"001.json"))
               for f in os.listdir(workdir)
-              if not isfile(join(workdir,f))
-                 and isfile(join(workdir,f,"001.json"))
+              if not isfile("/".join([workdir,f]))
+                 and isfile("/".join([workdir,f,"001.json"]))
              ]
   print ("found vuedirs:"); print (vuedirs)
-  vuedict = { js['slot']: js for js in vuedirs }
+  vuedict = { js['name']: js for js in vuedirs }
   print ("returning vuedict:"); print (vuedict)
   return vuedict
 
