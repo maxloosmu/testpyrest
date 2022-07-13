78729327db55f8d415d8be28fbf8f8edf3e9c0fd
Max Loo <maxloo@smu.edu.sg>
2022-07-13 13:06:19 +0800

    changed v8k.out, v8ktest

--- pyrest/v8ktest.py
+++ pyrest/v8ktest.py
@@ -7,17 +7,21 @@ spreadsheetId = "ssid"
 sheetId = "sid"
 
 targetFolder = "/home/maxloo/pyrest/temp/workdir/"+uuid+"/"+spreadsheetId+"/"+sheetId+"/"
-targetFile = datetime.datetime.utcnow().strftime("%Y%m%dT%H%M%S.%fZ") + ".csv"
-targetPath = targetFolder + targetFile
 Path(targetFolder).mkdir(parents=True, exist_ok=True)
 
 uuidssfolder = temp_dir + "workdir/" + uuid + "/" + spreadsheetId + "/" + sheetId
-createFiles = "natural4-exe --workdir=/home/maxloo/pyrest/temp/workdir --uuiddir=" + uuid + "/" + spreadsheetId + "/" + sheetId + " " + "/home/maxloo/pyrest/test.csv"
-nl4exe = os.system(createFiles)
+# createFiles = "natural4-exe --workdir=/home/maxloo/pyrest/temp/workdir --uuiddir=" + uuid + "/" + spreadsheetId + "/" + sheetId + " " + "/home/maxloo/pyrest/test.csv"
+# nl4exe = os.system(createFiles)
+createFiles2 = ["natural4-exe", 
+                  "--workdir=/home/maxloo/pyrest/temp/workdir", 
+                  "--uuiddir=" + uuid + "/" + spreadsheetId + "/" + sheetId, 
+                  "/home/maxloo/pyrest/test.csv"]
+nl4exe2 = subprocess.run(createFiles2, check=True, timeout=5, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 v8kargs = ["python", "/home/maxloo/vue-pure-pdpa/bin/v8k", "up",
                "--uuid=" + uuid,
                "--ssid=" + spreadsheetId,
                "--sheetid=" + sheetId,
                uuidssfolder + "/purs/LATEST.purs"]
-os.system(" ".join(v8kargs) + " > " + "/home/maxloo/pyrest/temp/v8k.out");
+os.system(" ".join(v8kargs) + " > " + "/home/maxloo/pyrest/v8k.out");
 
